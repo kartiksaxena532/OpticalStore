748e1e0aaaa0bd332f81a743f6ef61014ece5807
Kartik Saxena <karrtiksaxena@gmail.com>
2023-09-28 20:31:37 +0530

    new

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const Footer = () => {
   return (
-    <footer className='text-white '>
+    <footer className='text-white-800 flex-between body-text w-full gap-y-10 border-t-2 border-black-400 bg-black-100 px-20 py-12 max-md:flex-col '>
       <p className=''>
         Copyright © 2023 Deepak Opticals | All Rights Reserved 📱  
       </p> 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,16 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white'>
-   <div className='flex-between max-w-screen-2xl w-full mx-auto px-6 xs:px-8 sm:px-16 '>
+   <div className='flex-between max-w-screen-2xl w-full mx-auto px-6 xs:px-8 sm:px-16  '>
+    
 <Link href="/">
-  <Image src='/jsm-logo.svg' 
+<div className='flex-center'>
+  <Image src='/deeepak.jpg' 
   width={55}
   height={40}
   alt='Deepak Optical Logo'/>
+  <p  className='px-2 font-semibold text-gradient_blue-yellow text-2xl '> Deepak Opticals</p>
+  </div>
 </Link>
 
 <Image src='/hamburger-menu.svg' 
@@ -18,7 +22,7 @@ width={30}
 height={30}
 alt='hamburger'/>
 <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-10'>
-<li className='body-text text-gradient_blue-purple !font-bold'>
+<li className='body-text text-gradient_blue-yellow1 !font-bold'>
 <Link href="https://maps.app.goo.gl/nMwT3XG6ZZGwi5iv6" target='blank'>
 Visit Us!
 </Link>
