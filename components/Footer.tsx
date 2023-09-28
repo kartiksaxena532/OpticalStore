0008ae0f9ebfe09312dcea8b7100a1178d2e8308
Kartik Saxena <karrtiksaxena@gmail.com>
2023-09-29 00:01:15 +0530

    hero section update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='text-white-800 flex-between body-text w-full gap-y-10 border-t-2 border-black-400 bg-black-100 px-20 py-12 max-md:flex-col '>
       <p className=''>
-        Copyright © 2023 Deepak Opticals | All Rights Reserved 📱  
+        Copyright © 2023 Deepak Opticals😎 | All Rights Reserved ✅ 
       </p> 
       <div className='flex gap-x-9'>
         <Link href="/"> Terms & Conditions      
