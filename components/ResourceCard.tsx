eca4ffdd0c49ab897f354dc9a8edc364f9bba738
Kartik Saxena <karrtiksaxena@gmail.com>
2024-05-11 22:36:47 +0530

    fixed a bug

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/new/card';
 
 interface Props {
   id: string;
